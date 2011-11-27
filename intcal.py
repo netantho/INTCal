241381f991cb412d1d9cc36193f35fb03547484a
Anthony VEREZ <netantho@gmail.com>
2011-11-27 11:19:40 +0100

    Ajout de types de cours

--- intcal.py
+++ intcal.py
@@ -43,7 +43,7 @@ class Intcal:
 		"""Extrait les types (Cours Magistral, TP, TD, ...) du cours"""
 		self.types = []
 		for i in range(1, len(self.texte_nl)):
-			regex = re.search("TP|TD|Examen|Conférence|Cours Magistral|Présentation|Point de Rencontre|Projet|Formation en ligne|Soutenance", self.texte_nl[i].encode("utf-8"))
+			regex = re.search("TP|TD|Examen|Conférence|Cours Magistral|Présentation|Point de Rencontre|Projet|Formation en ligne|Soutenance|Cours de Langue|Langues|Cours Professeur|Cours Intégré", self.texte_nl[i].encode("utf-8"))
 			if (regex and not self.texte_nl[i] in self.groupes):
 				self.types.append(self.texte_nl[i])
 			else:
